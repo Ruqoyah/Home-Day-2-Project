d22dd6ff6305f5c32870db60f5421efee5558a03
Ruqoyah <rukayatodukoya123@gmail.com>
2017-01-10 16:18:05 +0100

    Public API

---
+++ gitAPI/gitHubAPI.js
@@ -0,0 +1,20 @@
+var GitHubApi = require("github");
+
+var github = new GitHubApi({
+    debug: true,
+    protocol: "https",
+    host: "api.github.com", 
+    pathPrefix: "", 
+    headers: {
+        "user-agent": "rkterungwa16" 
+    },
+    Promise: require('bluebird'),
+    followRedirects: false, 
+    timeout: 5000
+});
+
+github.users.getFollowingForUser({
+    username: "rkterungwa16"
+}, function(err, res) {
+    console.log(JSON.stringify(res));
+});
